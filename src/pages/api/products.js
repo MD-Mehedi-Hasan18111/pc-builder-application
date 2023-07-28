b1ad74028b64bc4ff3e548378cf16a8fc822615a
MD Mehedi Hasan <md@bursement.com>
2023-07-28 23:42:48 +0600

    Product API modified

--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -1,5 +1,6 @@
-import {MongoClient, ServerApiVersion} from "mongodb";
-const uri = `mongodb+srv://PC_BUILDER:117et8yIz6tyYpZW@cluster0.4g4am.mongodb.net/?retryWrites=true&w=majority`;
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = `mongodb+srv://PC_BUILDER:117et8yIz6tyYpZW@cluster0.yai2s.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -10,20 +11,21 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function dbConnect() {
+async function run(req, res) {
   try {
     await client.connect();
-
+    console.log("Database connected");
     const productsCollection = client.db("pc_builder").collection("components");
 
-    // if (req.method === "GET") {
-    //   const products = await productsCollection.find({}).toArray();
-    //   res.send({message: "success", status: 200, data: products});
-    // }
+    if (req.method === "GET") {
+      const products = await productsCollection.find({}).toArray();
+      res.send({ message: "success", status: 200, data: products });
+    }
     return productsCollection;
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-export default dbConnect;
+
+export default run;
